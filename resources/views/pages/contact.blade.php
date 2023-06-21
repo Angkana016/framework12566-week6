52f1a8d6b0e5a801de433128e6b756e41e93a629
Angkana016 <WannaratHnun@gmail.com>
2023-06-21 15:30:06 +0700

    first app

---
+++ resources/views/pages/contact.blade.php
@@ -0,0 +1,6 @@
+@extends('master')
+
+@section('title','Contact page')
+@section('content')
+    <p>This Contact contend</p>
+@endsection
---
+++ resources/views/pages/home.blade.php
@@ -0,0 +1,6 @@
+@extends('master')
+
+@section('title','Homeb page')
+@section('content')
+    <p>This home contend</p>
+@endsection
